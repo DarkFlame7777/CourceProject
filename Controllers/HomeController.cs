7024ac6a346f7803104b1b8566c595f612f68e62
DarkFlame7777 <player859@yandex.by>
2026-03-13 00:12:59 +0300

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,31 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using MyProject.Models;
+using Microsoft.EntityFrameworkCore;
+using MyProject.Data;
 
 namespace MyProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
-        public IActionResult Index() => View();
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var inventories = await _dbContext.Inventories
+                .Include(i => i.Creator)
+                .OrderByDescending(i => i.CreatedAt)
+                .Take(50)
+                .ToListAsync();
 
+            return View(inventories);
+        }
 
     }
 }
