77c322d18fd460c22a4ea643d82a7b8bf1f28d68
DarkFlame7777 <player859@yandex.by>
2026-03-12 23:27:43 +0300

    Add files via upload

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,6 +7,8 @@ namespace MyProject.Data
     {
         public DbSet<User> Users { get; set; }
         public DbSet<EmailToken> EmailTokens { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
+        public DbSet<Item> Items { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
