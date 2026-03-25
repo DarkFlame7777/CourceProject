ce65d5264c7e5b5da1a14f8c108de0aaac1553ba
DarkFlame7777 <player859@yandex.by>
2026-03-25 06:56:20 +0300

    Add files via upload

---
+++ Services/Implementions/InventoryService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyProject.Data;
+using MyProject.Models.Entities;
+using MyProject.Services.Abstractions;
+
+namespace MyProject.Services.Implementions
+{
+    public class InventoryService : IInventoryService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public InventoryService(ApplicationDbContext dbcontext)
+        {
+            _dbContext = dbcontext;
+        }
+
+        public async Task<List<Inventory>> GetInventory(int userId)
+            => await _dbContext.Inventories
+                .Where(i => i.CreatorId == userId)
+                .OrderByDescending(i => i.CreatedAt)
+                .ToListAsync();
+
+
+    }
+}
---
+++ Services/Implementions/UserService.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using MyProject.Data;
+using MyProject.Models.Entities;
+using MyProject.Models.ViewModels.Auth;
+using MyProject.Services.Abstractions;
+using System.Security.Claims;
+
+namespace MyProject.Services.Implementions
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IPasswordHasher _passwordHasher;
+
+        public UserService(ApplicationDbContext dbContext, IPasswordHasher passwordHasher)
+        {
+            _dbContext = dbContext;
+            _passwordHasher = passwordHasher;
+        }
+
+        public async Task<User?> GetUserByIdAsync(int id)
+            => await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+            => await _dbContext.Users.FirstOrDefaultAsync(u => u.Email == email);
+
+        public async Task<User> CreateUserAsync(RegisterViewModel model)
+        {
+            var user = CreateModelUser(model.Username, model.Email, model.Password);
+            _dbContext.Users.Add(user);
+            await _dbContext.SaveChangesAsync();
+            return user;
+        }
+
+        private User CreateModelUser(string username, string email, string password)
+            => new User
+            {
+                Username = username,
+                Email = email,
+                PasswordHash = _passwordHasher.HashPassword(password)
+            };
+
+        public async Task ConfirmEmailAsync(User user)
+        {
+            user.EmailConfirmed = true;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task ChangePasswordAsync(User user, string newPassword)
+        {
+            user.PasswordHash = _passwordHasher.HashPassword(newPassword);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        private List<Claim> GetUserClaims(User user)
+            => new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+        public ClaimsIdentity GetIdentity(User user)
+            => new ClaimsIdentity(GetUserClaims(user), 
+                CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+}
