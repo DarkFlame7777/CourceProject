4d81f108569a28ba76bd6a31bbc6f8b77dc2761b
DarkFlame7777 <player859@yandex.by>
2026-03-25 06:55:39 +0300

    Add files via upload

---
+++ Services/Abstractions/IInventoryService.cs
@@ -0,0 +1,9 @@
+﻿using MyProject.Models.Entities;
+
+namespace MyProject.Services.Abstractions
+{
+    public interface IInventoryService
+    {
+        Task<List<Inventory>> GetInventory(int userId);
+    }
+}
---
+++ Services/Abstractions/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using MyProject.Models.Entities;
+using MyProject.Models.ViewModels.Auth;
+using System.Security.Claims;
+
+namespace MyProject.Services.Abstractions
+{
+    public interface IUserService
+    {
+        Task<User?> GetUserByIdAsync(int id);
+        Task<User?> GetUserByEmailAsync(string email);
+
+        Task<User> CreateUserAsync(RegisterViewModel model);
+        Task ConfirmEmailAsync(User user);
+        Task ChangePasswordAsync(User user, string newPassword);
+
+        ClaimsIdentity GetIdentity(User user);
+    }
+}
